a9217ac71c0df6ab3eb618c2cb5fbdd544b44ff7
bssrdf <merlintiger@hotmail.com>
2016-08-07 12:15:36 -0400

    initial version of mono bicubic

--- monotonic_bicubic.py
+++ monotonic_bicubic.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Sun Aug 07 01:13:51 2016
+
+@author: bzhao
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Fri Jul 29 23:40:17 2016
 
 @author: bzhao
@@ -8,9 +15,12 @@ import numpy as np
 from scipy import interpolate
 import matplotlib.pyplot as plt
 
-def cubic_mono(xi, yi, xnew, mono=False):
+def bicubic_mono(xi, yi, zi, xnew, ynew, mono=False):
 
-   ynew=np.zeros(xnew.shape)
+   assert len(xnew.shape) == 2
+   assert len(ynew.shape) == 2
+      
+   znew=np.zeros(xnew.shape)
    for n in range(0, xnew.shape[0]):
         i = np.searchsorted(xi, xnew[n])-1
         t = (xnew[n]-xi[i])/(xi[i+1]-xi[i])
@@ -45,32 +55,33 @@ def cubic_mono(xi, yi, xnew, mono=False):
         ynew[n]= h00*yi[i] + h10*(xi[i+1]-xi[i])*s1 + h01*yi[i+1] + h11*(xi[i+1]-xi[i])*s2
         #print n, t, h00, h01, h10, h11, ynew[n]
         print n, xnew[n], ynew[n], s1, s2
-   return ynew
+   return znew
 
 
-xi=np.array([7.99, 8.09, 8.19, 8.7, 9.2, 10.0, 11.0, 12, 15, 20])
-yi=np.array([0.0, 2.76429e-5, 4.37498e-2, 0.169183, 0.469428, 0.943740, 0.8497, 0.998636, 
-             0.999919, 0.999994])
+xi=np.arange(0, 5.2, 0.2)
+yi=np.arange(0, 5.2, 0.2)
+xin=np.arange(0.1, 5.0, 0.05)
+yin=np.arange(0.1, 5.0, 0.1)
 
-f1 = interpolate.interp1d(xi, yi, kind='cubic')
-f2 = interpolate.interp1d(xi, yi)
-f3 = interpolate.PchipInterpolator(xi, yi)
-xnew=np.arange(8, 20, 0.1)
-y1new=f1(xnew)
-y2new=f2(xnew)
-y3new=f3(xnew)
 
-y4new= cubic_mono(xi, yi, xnew)
-y5new= cubic_mono(xi, yi, xnew, mono=True)
+
+x,y=np.meshgrid(xi, yi)
+xn,yn=np.meshgrid(xin, yin)
+
+zi = np.sin(x)*np.cos(y)
+
+f1=interpolate.RectBivariateSpline(xi, yi, zi)
+f2=interpolate.interp2d(xi, yi, zi, kind='cubic')
+z1 = f1(xin, yin)
+#z1 = z1.T
+z2 = f2(xin, yin)
+
 
 plt.figure()
-
-plt.plot(xi, yi,'ko')
-#plt.plot(xnew, y1new)
-plt.plot(xnew, y2new,'b')
-plt.plot(xnew, y3new,'g')
-plt.plot(xnew, y4new,'r')
-plt.plot(xnew, y5new,'m')
-plt.ylim(0, 1.1)
-plt.xlim(7.9, 20)
+plt.subplot(2,2,1)
+plt.pcolormesh(x, y, zi)
+plt.subplot(2,2,2)
+plt.pcolormesh(xn, yn, z1)
+plt.subplot(2,2,3)
+plt.pcolormesh(xn, yn, z2)
 plt.show()
